80920c1fd442d8b2ccb4b09fdd23c9489762e0d7
Duane Dieterich <duane.dieterich+github@gmail.com>
2025-01-23 18:36:18 -0600

    Successful Build OnPrem_Feee234

--- builds/OnPrem_Free234/build.sh
+++ builds/OnPrem_Free234/build.sh
@@ -43,6 +43,7 @@ PDB_SYSTEM="SYSTEM/${SYS_PASS}@OnPrem_Free234_${PDB_NAME}"
 
 ########################################
 
+capture_version
 build_init
 for BTYPE in 'ss_src' 'custom_data_load' 'grbsrc' 'grbsdo' 'grbxrep' 'ss_grb'
 do
--- builds/OnPrem_Free234/build_initialize.sql
+++ builds/OnPrem_Free234/build_initialize.sql
@@ -4,14 +4,12 @@
 --
 -- Command Line Parameters:
 --   1 - PDB_NAME: Name of the Pluggable Database
---   2 - DB_LOGIN: Connect String for SYS in the Container Database
 --
 
 WHENEVER SQLERROR EXIT SQL.SQLCODE
 WHENEVER OSERROR EXIT
 
 define PDB_NAME="&1."
-define DB_LOGIN="&2."
 
 set linesize 2499
 set trimspool on
@@ -20,7 +18,14 @@ set verify off
 set echo off
 set timing on
 
-@"../util/new_session.sql" "&DB_LOGIN." "" ""
+set serveroutput on size unlimited format wrapped
+select 'user: ' || u.username ||
+       ', db: ' || d.name ||
+       ', con: ' || sys_context('USERENV', 'CON_NAME') ||
+       ', tstmp: ' || systimestamp   CONNECTION
+ from  v$database d
+ cross join user_users u;
+
 @"../util/create_pdb.sql" "&PDB_NAME." "" ""
 
 exit
